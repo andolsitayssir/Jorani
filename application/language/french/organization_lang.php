f6cb7ba6e40998f8f94e21668499049878e70dac
tayssir andolsi <landolssitayssir26@gmail.com>
2024-08-07 11:22:25 +0100

    commit 3

--- application/language/french/organization_lang.php
+++ application/language/french/organization_lang.php
@@ -17,9 +17,9 @@ $lang['organization_index_thead_lastname'] = 'Nom';
 $lang['organization_index_thead_email'] = 'E-mail';
 $lang['organization_index_button_add_employee'] = 'Attacher un employé';
 $lang['organization_index_button_remove_employee'] = 'Détacher un employé';
-$lang['organization_index_button_select_supervisor'] = 'Selectionner';
-$lang['organization_index_title_supervisor'] = 'Superviseur';
-$lang['organization_index_description_supervisor'] = 'Si vous choisissez un superviseur, il recevra une copie de toutes les demandes acceptées et rejetées des employés de cette entité.';
+$lang['organization_index_button_select_supervisor'] = 'Ajouter un superviseur';
+$lang['organization_index_title_supervisor'] = 'Superviseurs';
+$lang['organization_index_description_supervisor'] = 'Si vous ajoutez un superviseur, il recevra une copie de toutes les demandes acceptées et rejetées des employés de cette entité.';
 
 $lang['organization_index_popup_supervisor_title'] = 'Selectionner un Superviseur';
 $lang['organization_index_popup_supervisor_button_ok'] = 'OK';
